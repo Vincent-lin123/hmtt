c1015389c5aeb923abcf102ba018cc216de3c1ba
vincent <15021026135@163.com>
2021-07-06 20:03:22 +0800

    first commit

--- src/main.js
+++ src/main.js
@@ -2,7 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import './styles/index.less'
+import Vant from 'vant'
+import 'vant/lib/index.css'
+// 加载动态设置REM基准值
+import 'amfe-flexible'
 
+Vue.use(Vant)
 Vue.config.productionTip = false
 
 new Vue({
